d51a22d9026d685b91e0b22b9fdc8677a9a91ae1
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-05-21 12:50:29 +0900

    Disallow editing other's tweets

--- my-app/resources/views/tweet/index.blade.php
+++ my-app/resources/views/tweet/index.blade.php
@@ -12,35 +12,39 @@
 <body>
     <h1>つぶやきアプリ</h1>
     @auth
-    <div>
-        @if (session('feedback.success'))
-            <p style="color: green;">{{ session('feedback.success') }}</p>
-        @endif
-        <p>投稿フォーム</p>
-        <form action="{{ route('tweet.create') }}" method="post">
-            @csrf
-            <label for="tweet-content">つぶやき</label>
-            <span>140文字まで</span>
-            <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
-            @error('tweet')
-                <p style="color: red;">{{ $message }}</p>
-            @enderror
-            <button type="submit">投稿</button>
-        </form>
-    </div>
+        <div>
+            @if (session('feedback.success'))
+                <p style="color: green;">{{ session('feedback.success') }}</p>
+            @endif
+            <p>投稿フォーム</p>
+            <form action="{{ route('tweet.create') }}" method="post">
+                @csrf
+                <label for="tweet-content">つぶやき</label>
+                <span>140文字まで</span>
+                <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
+                @error('tweet')
+                    <p style="color: red;">{{ $message }}</p>
+                @enderror
+                <button type="submit">投稿</button>
+            </form>
+        </div>
     @endauth
     <div>
         @foreach ($tweets as $tweet)
             <details>
                 <summary>{{ $tweet->content }} by {{ $tweet->user->name }}</summary>
-                <div>
-                    <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
-                    <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id ]) }}" method="post">
-                        @method('DELETE')
-                        @csrf
-                        <button type="submit">削除</button>
-                    </form>
-                </div>
+                @if (\Illuminate\Support\Facades\Auth::id() === $tweet->user_id)
+                    <div>
+                        <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+                        <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit">削除</button>
+                        </form>
+                    </div>
+                @else
+                    編集できません
+                @endif
             </details>
         @endforeach
     </div>
