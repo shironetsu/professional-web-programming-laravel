f156106e9ccb619e885a0628faaaba725b1edfb9
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-05-22 23:47:03 +0900

    Send mail when new user is introduced

---
+++ my-app/app/Mail/NewUserIntroduction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NewUserIntroduction extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    //このクラスのプロパティはbladeテンプレート中でそのまま使える。
+    public $subject = '新しいユーザーが追加されました！';
+    public User $toUser;
+    public User $newUser;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $toUser, User $newUser)
+    {
+        $this->toUser = $toUser;
+        $this->newUser = $newUser;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('email.new_user_introduction');
+    }
+}
