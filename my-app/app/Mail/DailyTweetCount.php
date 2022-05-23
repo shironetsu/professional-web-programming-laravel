c1edd1622e18fa7bda946597d7c4f24c22c6dd10
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-05-23 23:35:08 +0900

    Send daily-shceduled mail

---
+++ my-app/app/Mail/DailyTweetCount.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+//use Illuminate\Support\Facades\Log;
+
+class DailyTweetCount extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public User $toUser;
+    public int $count;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $toUser, int $count)
+    {
+        $this->toUser = $toUser;
+        $this->count = $count;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        //Log::debug('DailyTweetCount::build');
+        return $this->subject("昨日は{$this->count}件のつぶやきが追加されました！")
+            ->markdown('email.daily_tweet_count');
+    }
+}
