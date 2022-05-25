950f459b2525358a9c297f8c2eae7f1d2893f567
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-05-25 21:20:29 +0900

    Remove tweet with images

--- my-app/app/Http/Controllers/Tweet/DeleteController.php
+++ my-app/app/Http/Controllers/Tweet/DeleteController.php
@@ -22,9 +22,7 @@ class DeleteController extends Controller
         if (!$tweetService->checkOwnTweet($request->user()->id, $tweetId)) {
             throw new AccessDeniedHttpException();
         }
-        $tweet = Tweet::where('id', $tweetId)->firstOrFail();
-        $tweet->delete();
-        //Tweet::destroy($tweetId); //でも可
+        $tweetService->deleteTweet($tweetId);
         return redirect()
             ->route('tweet.index')
             ->with('feedback.success', 'つぶやきを削除しました');
